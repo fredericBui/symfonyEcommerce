4d42520d012c5746d5602605aaeee0b82d63e81e
Frederic <bui.frederic.pro@gmail.com>
2022-07-05 02:14:31 +0200

    redirect / to home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
