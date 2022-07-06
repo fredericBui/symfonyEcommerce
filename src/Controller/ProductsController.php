0e1bd0056e1658bfde8c2d4d426ab457e2792fff
Frederic <bui.frederic.pro@gmail.com>
2022-07-06 22:50:43 +0200

    filter products with form get

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoriesRepository;
 use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,11 +11,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductsController extends AbstractController
 {
     #[Route('/products', name: 'app_products')]
-    public function index(ProductsRepository $productsRepository): Response
+    public function index(ProductsRepository $productsRepository, CategoriesRepository $categoriesRepository): Response
     {
+
+        if(isset($_GET["filter"])){
+            if($_GET["filter"] == "all"){
+                $products = $productsRepository->findAll();
+            }else{
+                $products = $productsRepository->findBy(array('category' => $_GET["filter"]),array('name' => 'ASC'));
+            }
+        }else{
+            $products = $productsRepository->findAll();
+        }
+
+
         return $this->render('products/index.html.twig', [
             'controller_name' => 'ProductsController',
-            'products' => $productsRepository->findAll()
+            'products' => $products,
+            'categories' => $categoriesRepository->findAll()
         ]);
     }
 }
